24fb2779d2d1b2e26ac537f54c3e595339e3aedc
andrea longhi <andrea@spaghetticode.it>
2013-05-03 09:15:10 +0200

    add assets pipeline slides and code

---
+++ rails_extras/assets_pipeline/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'eb2c9e9e43d1aab9d06ef477aae0f9996eaee5af5a4d12e2a51fe7cdda4c7193df9ab01b9fe57e3f742ff7f1f1b1e1314c2302c22c7e50bb6048817cb020feef'
